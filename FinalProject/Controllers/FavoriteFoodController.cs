3960fbeda964fc6987ac6298b92fde344ce411fb
Brady Jacoby <143133514+Trimival@users.noreply.github.com>
2024-12-04 15:20:44 -0500

    Update FavoriteFoodController.cs
    cleaned up code, no code changes

--- FinalProject/Controllers/FavoriteFoodController.cs
+++ FinalProject/Controllers/FavoriteFoodController.cs
@@ -2,35 +2,29 @@
 using FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FinalProject.Controllers
-{
+namespace FinalProject.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class FavoriteFoodController : ControllerBase
-    {
+    public class FavoriteFoodController : ControllerBase {
         private readonly ILogger<FavoriteFoodController> _logger;
         private readonly IFavoriteFoodContextDAO _context;
 
-        public FavoriteFoodController(ILogger<FavoriteFoodController> logger, IFavoriteFoodContextDAO context)
-        {
+        public FavoriteFoodController(ILogger<FavoriteFoodController> logger, IFavoriteFoodContextDAO context) {
             _logger = logger;
             _context = context;
         }
             // GET: FavoriteFoods
             [HttpGet]
-            public async Task<ActionResult<IEnumerable<FavoriteFood>>> GetFavoriteFoods()
-            {
+            public async Task<ActionResult<IEnumerable<FavoriteFood>>> GetFavoriteFoods() {
                 var favoriteFoods = await _context.GetAllFavoriteFoodsAsync();
                 return Ok(favoriteFoods);
             }
 
             // GET: FavoriteFoods by Id
             [HttpGet("{id}")]
-            public async Task<ActionResult<FavoriteFood>> GetFavoriteFood(int id)
-            {
+            public async Task<ActionResult<FavoriteFood>> GetFavoriteFood(int id) {
                 var favoriteFood = await _context.GetFavoriteFoodByIdAsync(id);
-                if (favoriteFood == null)
-                {
+                if (favoriteFood == null) {
                     return NotFound();
                 }
 
@@ -39,18 +33,15 @@ namespace FinalProject.Controllers
 
             // POST: FavoriteFoods
             [HttpPost]
-            public async Task<ActionResult<FavoriteFood>> PostFavoriteFood(FavoriteFood favoriteFood)
-            {
+            public async Task<ActionResult<FavoriteFood>> PostFavoriteFood(FavoriteFood favoriteFood) {
                 await _context.AddFavoriteFoodAsync(favoriteFood);
                 return CreatedAtAction(nameof(GetFavoriteFood), new { id = favoriteFood.Id }, favoriteFood);
             }
 
             // PUT: FavoriteFoods
             [HttpPut("{id}")]
-            public async Task<IActionResult> PutFavoriteFood(int id, FavoriteFood favoriteFood)
-            {
-                if (id != favoriteFood.Id)
-                {
+            public async Task<IActionResult> PutFavoriteFood(int id, FavoriteFood favoriteFood) {
+                if (id != favoriteFood.Id) {
                     return BadRequest();
                 }
 
@@ -60,8 +51,7 @@ namespace FinalProject.Controllers
 
             // DELETE: FavoriteFoods
             [HttpDelete("{id}")]
-            public async Task<IActionResult> DeleteFavoriteFood(int id)
-            {
+            public async Task<IActionResult> DeleteFavoriteFood(int id) {
                 await _context.DeleteFavoriteFoodAsync(id);
                 return Ok(NoContent());
             }
