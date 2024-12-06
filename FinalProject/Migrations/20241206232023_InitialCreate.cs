39928749c9ad0b225c6804391446913d6d7f40b8
Morgan Montgomery <morgan.montgomery95@gmail.com>
2024-12-06 18:52:02 -0500

    Re-added call to create Members table

--- FinalProject/Migrations/20241206232023_InitialCreate.cs
+++ FinalProject/Migrations/20241206232023_InitialCreate.cs
@@ -57,8 +57,22 @@ namespace FinalProject.Migrations
                     table.PrimaryKey("PK_Hobbies", x => x.Id);
                 });
 
-            
-           
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CollegeProgram = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearInProgram = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
 
             migrationBuilder.InsertData(
                 table: "FavoriteFoods",
