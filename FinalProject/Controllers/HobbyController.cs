23a2c17c53dbc9d8278f54ee7f3f7e13dee14c43
Brady Jacoby <143133514+Trimival@users.noreply.github.com>
2024-12-05 19:08:58 -0500

    Update HobbyController.cs
    code cleanup, no major changes

--- FinalProject/Controllers/HobbyController.cs
+++ FinalProject/Controllers/HobbyController.cs
@@ -7,32 +7,27 @@ namespace FinalProject.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class HobbyController : ControllerBase
-    {
+    public class HobbyController : ControllerBase {
         private readonly ILogger<HobbyController> _logger;
         private readonly IHobbyContextDAO _context;
 
-        public HobbyController(ILogger<HobbyController> logger, IHobbyContextDAO context)
-        {
+        public HobbyController(ILogger<HobbyController> logger, IHobbyContextDAO context) {
             _logger = logger;
             _context = context;
         }
 
             // GET: Hobbies
             [HttpGet]
-            public async Task<ActionResult<IEnumerable<Hobby>>> GetHobbies()
-            {
+            public async Task<ActionResult<IEnumerable<Hobby>>> GetHobbies() {
                 var hobbies = await _context.GetAllHobbiesAsync();
                 return Ok(hobbies);
             }
 
             // GET: Hobbies
             [HttpGet("{id}")]
-            public async Task<ActionResult<Hobby>> GetHobby(int id)
-            {
+            public async Task<ActionResult<Hobby>> GetHobby(int id) {
                 var hobby = await _context.GetHobbyByIdAsync(id);
-                if (hobby == null)
-                {
+                if (hobby == null) {
                     return NotFound();
                 }
 
@@ -41,18 +36,15 @@ namespace FinalProject.Controllers
 
             // POST: Hobbies
             [HttpPost]
-            public async Task<ActionResult<Hobby>> PostHobby(Hobby hobby)
-            {
+            public async Task<ActionResult<Hobby>> PostHobby(Hobby hobby) {
                 await _context.AddHobbyAsync(hobby);
                 return CreatedAtAction(nameof(GetHobby), new { id = hobby.Id }, hobby);
             }
 
             // PUT: Hobbies
             [HttpPut("{id}")]
-            public async Task<IActionResult> PutHobby(int id, Hobby hobby)
-            {
-                if (id != hobby.Id)
-                {
+            public async Task<IActionResult> PutHobby(int id, Hobby hobby) {
+                if (id != hobby.Id) {
                     return BadRequest();
                 }
 
@@ -62,8 +54,7 @@ namespace FinalProject.Controllers
 
             // DELETE: Hobbies
             [HttpDelete("{id}")]
-            public async Task<IActionResult> DeleteHobby(int id)
-            {
+            public async Task<IActionResult> DeleteHobby(int id){
                 await _context.DeleteHobbyAsync(id);
                 return Ok(NoContent());
             }
