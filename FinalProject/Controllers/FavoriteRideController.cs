92c78bcefc854677efa257d1706b5d5260a67b12
Brady Jacoby <143133514+Trimival@users.noreply.github.com>
2024-12-05 19:07:48 -0500

    Update FavoriteRideController.cs
    Code clean, no major changes

--- FinalProject/Controllers/FavoriteRideController.cs
+++ FinalProject/Controllers/FavoriteRideController.cs
@@ -2,35 +2,29 @@
 using FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FinalProject.Controllers
-{
+namespace FinalProject.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class FavoriteRideController : ControllerBase
-    {
+    public class FavoriteRideController : ControllerBase {
         private readonly ILogger<FavoriteRideController> _logger;
         private readonly IFavoriteRideContextDAO _context;
 
-        public FavoriteRideController(ILogger<FavoriteRideController> logger, IFavoriteRideContextDAO context)
-        {
+        public FavoriteRideController(ILogger<FavoriteRideController> logger, IFavoriteRideContextDAO context) {
             _logger = logger;
             _context = context;
         }
             // GET: FavoriteRides
             [HttpGet]
-            public async Task<ActionResult<IEnumerable<FavoriteRide>>> GetFavoriteRides()
-            {
+            public async Task<ActionResult<IEnumerable<FavoriteRide>>> GetFavoriteRides() {
                 var favoriteRides = await _context.GetAllFavoriteRidesAsync();
                 return Ok(favoriteRides);
             }
 
             // GET: FavoriteRides
             [HttpGet("{id}")]
-            public async Task<ActionResult<FavoriteRide>> GetFavoriteRide(int id)
-            {
+            public async Task<ActionResult<FavoriteRide>> GetFavoriteRide(int id) {
                 var favoriteRide = await _context.GetFavoriteRideByIdAsync(id);
-                if (favoriteRide == null)
-                {
+                if (favoriteRide == null) {
                     return NotFound();
                 }
 
@@ -47,10 +41,8 @@ namespace FinalProject.Controllers
 
             // PUT: FavoriteRides
             [HttpPut("{id}")]
-            public async Task<IActionResult> PutFavoriteRide(int id, FavoriteRide favoriteRide)
-            {
-                if (id != favoriteRide.Id)
-                {
+            public async Task<IActionResult> PutFavoriteRide(int id, FavoriteRide favoriteRide) {
+                if (id != favoriteRide.Id) {
                     return BadRequest();
                 }
 
@@ -60,8 +52,7 @@ namespace FinalProject.Controllers
 
             // DELETE: FavoriteRides
             [HttpDelete("{id}")]
-            public async Task<IActionResult> DeleteFavoriteRide(int id)
-            {
+            public async Task<IActionResult> DeleteFavoriteRide(int id) {
                 await _context.DeleteFavoriteRideAsync(id);
                 return Ok(NoContent());
             }
