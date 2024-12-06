34fb61ee1e553137171cb8a673369441c86e71f4
Brady Jacoby <143133514+Trimival@users.noreply.github.com>
2024-12-06 17:39:43 -0500

    Update TeamMembers.cs
    changed datatype for birthday from string to datetime

--- FinalProject/Models/TeamMembers.cs
+++ FinalProject/Models/TeamMembers.cs
@@ -1,11 +1,9 @@
-﻿namespace FinalProject.Models
-{
-    public class TeamMember
-    {
+﻿namespace FinalProject.Models {
+    public class TeamMember {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Birthdate { get; set; }
+        public DateTime Birthdate { get; set; }
         public string CollegeProgram { get; set; }
         public string YearInProgram { get; set; }
     }
